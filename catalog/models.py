7fe23a50c896f62529b19c6670ec7d1efe76223a
Javier Barrera <js.barrerat1@uniandes.edu.co>
2024-11-01 16:05:59 -0500

    using models in django

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,180 @@
 from django.db import models
 
+from django.urls import reverse
+from django.db.models import UniqueConstraint
+from django.db.models.functions import Lower
+
+import uuid
+
 # Create your models here.
+
+# important Metadata for the model
+# (https://docs.djangoproject.com/en/5.0/ref/models/options/)
+# - db_table = "table name"
+# - db_table_comment = "Description of the table"
+# > Contraints
+# (https://docs.djangoproject.com/en/5.0/ref/models/constraints/)
+# - CheckConstraint(check=Q(param__cond=18), name='check name')
+
+class Genre(models.Model):
+  """Model representing a book genre."""
+  name = models.CharField(
+    max_length=200,
+    unique=True,
+    help_text='Enter a book genre (e.g. Science Fiction)'
+  )
+
+  id = models.AutoField(
+    primary_key=True,
+    unique=True,
+    help_text="Unique ID for a Genre"
+  )
+
+  def __str__(self):
+    """String for representing the Model object."""
+    return self.name
+  
+  def get_absolute_url(self):
+    """Returns the url to access a detail record for this genre."""
+    return reverse('genre-detail', args=[str(self.id)])
+  
+  class Meta:
+    db_table = "genre"
+
+    constraints = [
+      UniqueConstraint(
+        Lower('name'),
+        name='genre_name_case_insensitive_unique',
+        violation_error_message="Genre already exists (case insensitive match)"
+      )
+    ]
+
+class Language(models.Model):
+  """Model representing a Language (e.g. English, French, Japanese, etc.)"""
+  name = models.CharField(
+    max_length=200,
+    help_text="Enter the book's natural language (e.g. English, French, Japanese etc.)"
+  )
+
+  id = models.AutoField(
+    primary_key=True,
+    unique=True,
+    help_text="Unique ID for a Language"
+  )
+
+  def __str__(self):
+    """String for representing the Model object."""
+    return self.name
+  
+  def get_absolute_url(self):
+    """Returns the url to access a detail record for this language."""
+    return reverse('language-detail', args=[str(self.id)])
+
+class Book(models.Model):
+  """Model representing a book (but not a specific copy of a book)."""
+  id = models.AutoField(
+    primary_key=True,
+    unique=True,
+    help_text="Unique ID for a Book"
+  )
+
+  title = models.CharField(max_length=200)
+
+  author = models.ForeignKey(
+    'Author',
+    on_delete=models.RESTRICT,
+    null=True
+  )
+
+  summary = models.TextField(
+    max_length=1000,
+    help_text='Enter a brief description of the book'
+  )
+
+  isbn = models.CharField(
+    'ISBN',
+    max_length=13,
+    unique=True,
+    help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>'
+  )
+
+  genre = models.ManyToManyField(
+    Genre,
+    help_text='Select a genre for this book'
+  )
+
+  language = models.ForeignKey(
+    Language,
+    on_delete=models.SET_NULL,
+    null=True
+  )
+
+  def __str__(self):
+    """String for representing the Model object."""
+    return self.title
+  
+  def get_absolute_url(self):
+    """Returns the url to access a detail record for this book."""
+    return reverse('book-detail', args=[str(self.id)])
+
+class BookCopy(models.Model):
+  """Model representing a specific copy of a book (i.e. that can be borrowed from the library)."""
+  id = models.UUIDField(
+    primary_key=True,
+    default=uuid.uuid4,
+    help_text='Unique ID for this particular book across whole library'
+  )
+
+  book = models.ForeignKey(
+    Book,
+    on_delete=models.RESTRICT,
+    null=True
+  )
+
+  imprint = models.CharField(max_length=200)
+  due_back = models.DateField(null=True, blank=True)
+
+  LOAN_STATUS = (
+    ('m', 'Maintenance'),
+    ('o', 'On loan'),
+    ('a', 'Available'),
+    ('r', 'Reserved'),
+  )
+
+  status = models.CharField(
+    max_length=1,
+    choices=LOAN_STATUS,
+    blank=True,
+    default='m',
+    help_text='Book availability',
+  )
+
+  class Meta:
+    ordering = ['due_back']
+
+  def __str__(self):
+    """String for representing the Model object."""
+    return f'{self.id} ({self.book.title})'
+
+class Author(models.Model):
+  """Model representing an author."""
+  id = models.AutoField(
+    primary_key=True,
+    unique=True,
+    help_text="Unique ID for an Author"
+  )
+  first_name = models.CharField(max_length=100)
+  last_name = models.CharField(max_length=100)
+  date_of_birth = models.DateField(null=True, blank=True)
+  date_of_death = models.DateField('Died', null=True, blank=True)
+
+  class Meta:
+    ordering = ['last_name', 'first_name']
+  
+  def get_absolute_url(self):
+    """Returns the url to access a particular author instance."""
+    return reverse('author-detail', args=[str(self.id)])
+  
+  def __str__(self):
+    """String for representing the Model object."""
+    return f'{self.last_name}, {self.first_name}'
