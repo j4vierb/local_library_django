7878ea4c3e297a4ef319f4da8928fa39817b9bee
Javier Barrera <js.barrerat1@uniandes.edu.co>
2024-12-29 20:37:05 -0500

    finish half part of django tutorial 10

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Local library Django project
 
-This is my own implementation for the MDN tutorial series to learn [Django](https://developer.mozilla.org/en-US/docs/Learn/Server-side/Django). 
+This is my own implementation for the MDN tutorial series to learn [Django](https://developer.mozilla.org/en-US/docs/Learn/Server-side/Django).
+The tutorial is completed until "[Views that are restricted to logged in users](https://developer.mozilla.org/en-US/docs/Learn_web_development/Extensions/Server-side/Django/Testing)".
--- local_library/urls.py
+++ local_library/urls.py
@@ -21,15 +21,15 @@ from django.views.generic import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
 
+""" static(
+        settings.STATIC_URL,
+        document_root=settings.STATIC_ROOT
+    )[0], """
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('catalog/', include('catalog.urls')),
-    path('', RedirectView.as_view(url='catalog/', permanent=True)), 
-    static(
-        settings.STATIC_URL,
-        document_root=settings.STATIC_ROOT
-    )[0],
-
+    path('', RedirectView.as_view(url='catalog/', permanent=True)),
     path('accounts/', include('django.contrib.auth.urls'))
 ]
